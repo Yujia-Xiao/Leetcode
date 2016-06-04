b2a15a021cab22b2fa7650871b0e5294f488a8a5
YujiaXiao <xiao_samantha@yahoo.com>
2016-06-04 17:11:58 -0400

    Two Pointers Hard

---
+++ Two_Pointers/Find_the_Duplicated_Number.java
@@ -0,0 +1,34 @@
+/*
+287. Find the Duplicate Number My Submissions QuestionEditorial Solution
+Total Accepted: 29426 Total Submissions: 75086 Difficulty: Hard
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+Note:
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+
+Hide Company Tags Bloomberg
+Hide Tags Binary Search Array Two Pointers
+Hide Similar Problems (H) First Missing Positive (M) Single Number (M) Linked List Cycle II (M) Missing Number
+*/
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        if(nums.length<2)return -1;
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }
+        fast=0;
+        while(fast!=slow){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
+}
