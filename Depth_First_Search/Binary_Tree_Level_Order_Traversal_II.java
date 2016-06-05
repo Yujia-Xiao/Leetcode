d70ade9c0345a5fb7a915a0f892683ab9abac735
YujiaXiao <xiao_samantha@yahoo.com>
2016-06-05 15:28:55 -0400

    DFS

---
+++ Depth_First_Search/Binary_Tree_Level_Order_Traversal_II.java
@@ -0,0 +1,50 @@
+/*
+107. Binary Tree Level Order Traversal II My Submissions QuestionEditorial Solution
+Total Accepted: 83016 Total Submissions: 242183 Difficulty: Easy
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+Hide Tags Tree Breadth-first Search
+Hide Similar Problems (E) Binary Tree Level Order Traversal
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new LinkedList<List<Integer>>();
+        dfs(ans,root,0);
+        Collections.reverse(ans);
+        return ans;
+    }
+    
+    public void dfs(List<List<Integer>> ans, TreeNode root, int level){
+        if(root==null)return;
+        if(level==ans.size()){
+            List<Integer> list = new LinkedList<Integer>();
+            ans.add(list);
+        }
+        List<Integer> levelList = ans.get(level);
+        levelList.add(root.val);
+        dfs(ans,root.left,level+1);
+        dfs(ans,root.right,level+1);
+    }
+}
---
+++ Depth_First_Search/House_Robber_III.java
@@ -0,0 +1,49 @@
+/*
+337. House Robber III My Submissions QuestionEditorial Solution
+Total Accepted: 13559 Total Submissions: 35998 Difficulty: Medium
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+Example 1:
+     3
+    / \
+   2   3
+    \   \ 
+     3   1
+Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+Example 2:
+     3
+    / \
+   4   5
+  / \   \ 
+ 1   3   1
+Maximum amount of money the thief can rob = 4 + 5 = 9.
+Credits:
+Special thanks to @dietpepsi for adding this problem and creating all test cases.
+
+Hide Company Tags Uber
+Hide Tags Tree Depth-first Search
+Hide Similar Problems (E) House Robber (M) House Robber II
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int rob(TreeNode root) {
+    //half-dp and dfs thinking
+        if(root==null)return 0;
+        int l = rob(root.left);
+        int r = rob(root.right);
+        int subSum = 0;
+        if(root.left!=null)subSum+=rob(root.left.left)+rob(root.left.right);
+        if(root.right!=null)subSum+=rob(root.right.left)+rob(root.right.right);
+        return Math.max(l+r,subSum+root.val);
+    }
+}
